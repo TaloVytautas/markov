1c59408ae3348dd8df4137158ee4e83a4ef7ed90
Tomas <tomasly@chalmers.se>
2026-04-24 21:51:59 +0200

    Update kod.py

--- kod.py
+++ kod.py
@@ -4,4 +4,9 @@ data = np.load("trajectory.npy")
 
 [training_data, testing_data] = np.array_split(data, 2)
 
-print(testing_data)
+model = [[0]*100]*100
+
+model = np.array(model)
+
+print(model)
+pi = 0
