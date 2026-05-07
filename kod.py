97a3b9ab2b37cfa43f4c5f0cc2a47f8b81e16627
Tomas <tomasly@chalmers.se>
2026-05-07 19:24:07 +0200

    Update kod.py

--- kod.py
+++ kod.py
@@ -13,19 +13,54 @@ for current, next in zip(training_data[:-1], training_data[1:]):
 row_sum = np.sum(model, 1, keepdims=True)
 
 def normalise(matrix, sums, b):
-    matrix += b
-    sums += b*100
-    with np.errstate(divide="ignore"):
-        return np.where(sums == 0, 0, matrix/sums)
+    testing_matrix = matrix + b
+    testing_sums = sums + b*100
+    with np.errstate(divide="ignore", invalid="ignore"):
+        return np.where(testing_sums == 0, 0, testing_matrix/testing_sums)
 
 def testing(model, validation):
-    pi = 1
-    pi_log10 = 0
+    val_curr = validation[:-1] - 1
+    val_next = validation[1:] - 1
 
-    for current, next in zip(validation[:-1], validation[1:]):
-        pi *= model[current-1][next-1]
-        pi_log10 += np.log(model[current-1][next-1])/np.log(10)
+    probs = model[val_curr, val_next]
+
+    pi_log10 = np.sum(np.log10(probs))
+    pi = 10**pi_log10
 
     return pi, pi_log10
 
-print(testing(normalise(model, row_sum, 0), testing_data))
+def span(start, end, amount, matrix, sums, validation):
+    b_values = np.linspace(start, end, amount)
+    pi_values = []
+
+    for b in b_values:
+        pi, pi_log10 = testing(normalise(matrix, sums, b), validation)
+        pi_values.append((pi, pi_log10))
+    
+    return list(zip(b_values, pi_values))
+
+#Uppgift 1
+
+pi, pi_log10 = testing(normalise(model, row_sum, 0), testing_data)
+
+print("Pi:", pi)
+print("log10(Pi):", pi_log10)
+
+#Uppgift 2
+
+pi, pi_log10 = testing(normalise(model, row_sum, 0.2), testing_data)
+
+print("Pi:", pi)
+print("log10(Pi):", pi_log10)
+
+#Uppgift 3
+
+values = span(0, 1, 10000, model, row_sum, testing_data)
+
+greatest_pi_log10_tuple = max(values, key=lambda x: x[1][1])
+
+b_values = [item[0] for item in values]
+pi_log10_values = [item[1][1] for item in values]
+
+plt.plot(b_values, pi_log10_values)
+plt.show()
