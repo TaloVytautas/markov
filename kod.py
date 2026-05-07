96dba600787e46a4300e9099c165e256723d461c
Tomas <tomasly@chalmers.se>
2026-05-07 16:32:28 +0200

    Update kod.py

--- kod.py
+++ kod.py
@@ -10,7 +10,22 @@ model = np.zeros((100,100))
 for current, next in zip(training_data[:-1], training_data[1:]):
     model[current-1][next-1] += 1
 
-row_sum = np.sum(model, 0)
+row_sum = np.sum(model, 1, keepdims=True)
 
-for row in model/row_sum:
-    print(row)
+def normalise(matrix, sums, b):
+    matrix += b
+    sums += b*100
+    with np.errstate(divide="ignore"):
+        return np.where(sums == 0, 0, matrix/sums)
+
+def testing(model, validation):
+    pi = 1
+    pi_log10 = 0
+
+    for current, next in zip(validation[:-1], validation[1:]):
+        pi *= model[current-1][next-1]
+        pi_log10 += np.log(model[current-1][next-1])/np.log(10)
+
+    return pi, pi_log10
+
+print(testing(normalise(model, row_sum, 0), testing_data))
